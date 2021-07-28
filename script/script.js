8278dafc7fa31c91f96b934c252b67c8f169f47d
stormbringer88 <alessandronessi@msn.com>
2021-07-28 16:22:51 +0200

    using form

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
-// ask user name
-var userName = prompt("Insert your name: ");
-// ask user surname
-var surName = prompt("And your surname: ");
-// ask user favourite color
-var favouriteColor = prompt("What is your favourite color? "); 
-// concatenate name, surname and color with "40"
-var generatedPassword = userName + surName +favouriteColor + "40";
-// write the element created on the doc with the generated password
-document.getElementById('scriptTarget').innerText = generatedPassword;
+// getform data
+function formSubmit() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var favouriteColor = document.getElementById("favoriteColor").value;
+    var generatedPassword = firstName + lastName +favouriteColor + "40";
+    document.getElementById('scriptTarget').innerHTML = generatedPassword;
+    alert(generatedPassword);
+}
