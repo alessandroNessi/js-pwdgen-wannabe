3b00c8685b1b2316f14cedd1c58b09e1ded72741
stormbringer88 <alessandronessi@msn.com>
2021-07-28 16:54:35 +0200

    changed with input and labels

--- script/script.js
+++ script/script.js
@@ -4,6 +4,13 @@ function formSubmit() {
     var lastName = document.getElementById("lastName").value;
     var favouriteColor = document.getElementById("favoriteColor").value;
     var generatedPassword = firstName + lastName +favouriteColor + "40";
+    if( firstName == "" || lastName == "" || favouriteColor == ""){
+        alert("error, data are incolplete");
+        return
+    }
     document.getElementById('scriptTarget').innerHTML = generatedPassword;
-    alert(generatedPassword);
+    // document.getElementById("MyElement").classList.add('MyClass');
+    document.getElementById('scriptTarget').classList.remove('d-none');
+    // alert(generatedPassword);
 }
+// function copyText
